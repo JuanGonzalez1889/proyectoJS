ec4daca64e496fc939d176bec4b304a21f9b1f92
unknown <jgonzalez@decreditos.com>
2023-03-20 01:42:37 -0300

    iniciando proyecto js

---
+++ public/script.js
@@ -0,0 +1,28 @@
+var slides = document.querySelectorAll(".slide");
+var currentSlide = 0;
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+setInterval(nextSlide, 5000); // Cambiar de slide cada 5 segundos
+
+
+//reloj
+function updateTime() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  var timeString =
+    hours.toString().padStart(2, "0") +
+    ":" +
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
+  document.getElementById("clock").textContent = timeString;
+}
+
+setInterval(updateTime, 1000);
