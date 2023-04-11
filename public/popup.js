9d2dbbcf9016fc2494e2b1cb899c93b9c4770ec2
JuanGonzalez1889 <jgonzalez@decreditos.com>
2023-04-11 18:18:43 -0300

    agregando loader

--- public/popup.js
+++ public/popup.js
@@ -2,15 +2,29 @@ let popup = document.querySelector(".popup");
 let form = document.querySelector(".form");
 let okButton = document.querySelector("#okButton");
 
+
 function openPopup() {
   popup.style.display = "flex";
 }
 
+//function closePopup() {
+//  popup.style.display = "none";
+//  form.reset();
+//  let contenedor = document.getElementById("contenedor_carga");
+//  contenedor.style.visibility = "visible";
+//  contenedor.style.opacity = "1";
+//}
 function closePopup() {
+  let contenedorCarga = document.getElementById("contenedor_carga");
   popup.style.display = "none";
+  contenedorCarga.style.visibility = "visible";
+  contenedorCarga.style.opacity = "1";
   form.reset();
+  setTimeout(function () {
+    contenedorCarga.style.visibility = "hidden";
+    contenedorCarga.style.opacity = "0";
+  }, 1000); // 3000 es el tiempo en milisegundos (3 segundos)
 }
-
 form.addEventListener("submit", function (event) {
   event.preventDefault(); // evitar que el formulario se envíe automáticamente
   if (validarFormulario()) {
@@ -20,4 +34,10 @@ form.addEventListener("submit", function (event) {
   }
 });
 
-okButton.addEventListener("click", closePopup);
+okButton.addEventListener("click", closePopup)
+window.onload = function () {
+  let contenedor = document.getElementById("contenedor_carga");
+  contenedor.style.visibility = "hidden";
+  contenedor.style.opacity = "0";
+};
+
